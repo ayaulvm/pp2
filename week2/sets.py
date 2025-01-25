4cf5d746754bbf9532787388aeb5042bd609771f
Ayaulym Gizatulina <ayaulymgizatulina@Noutbuk-Ayaulym.local>
2025-01-26 00:47:42 +0500

    update

---
+++ week2/sets.py
@@ -0,0 +1,13 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+# Note: the set list is unordered, meaning: the items will appear in a random order.
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+print(set1)
+print(set2)
+print(set3)
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
